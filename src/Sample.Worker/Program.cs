bfb5566eea3ffeea1ce1c6986a6b41501b430764
Chris Patterson <chris@phatboyg.com>
2022-08-09 09:16:11 -0500

    Added log message to worker, only worker should rebuild/deploy.

--- src/Sample.Worker/Program.cs
+++ src/Sample.Worker/Program.cs
@@ -6,7 +6,12 @@ var host = Host.CreateDefaultBuilder(args)
     .UseSerilogConfiguration()
     .UseMassTransitConfiguration(x =>
     {
-        x.AddHandler(async (SubmitOrder order) => new OrderSubmissionAccepted(order.OrderId));
+        x.AddHandler(async (SubmitOrder order, ILogger<SubmitOrder> logger) =>
+        {
+            logger.LogInformation("Consuming Submit Order: {OrderId}", order.OrderId);
+
+            return new OrderSubmissionAccepted(order.OrderId);
+        });
     })
     .Build();
 
